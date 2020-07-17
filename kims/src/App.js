729e4a06ea64676bb507164affd7db47447e9e0f
kazimhr <53475165+kazimhr@users.noreply.github.com>
2020-07-17 07:28:21 +0500

    react app created

---
+++ kims/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+function App() {
+	return <div />;
+}
+
+export default App;
